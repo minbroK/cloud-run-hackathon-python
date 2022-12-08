ccdfb1b1ac98b25127506407de024263436af766
minbroK <86211743+minbroK@users.noreply.github.com>
2022-12-08 16:14:21 +0900

    오른쪽 위로 이동 수정

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def move():
     # TODO add your implementation here to replace the random response
     dims = request.json['arena']['dims']
 
-    my_state = request.json['arena']['state']['https://cloud-run-hackathon-python-og36l62xga-uc.a.run.app']
+    my_state = request.json['arena']['state']['https://cloud-run-hackathon-python-xp5leyhpkq-uc.a.run.app']
 
     all_state = request.json['arena']['state']
 
